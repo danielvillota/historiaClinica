9127d3610feafa01b8eba1a25116984c106a9bfe
Daniel Villota <danielfernandovillota@gmail.com>
2023-11-05 18:35:38 -0500

    add models

--- historia_clinica_api/models.py
+++ historia_clinica_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Historia(models.Model):
+    id=models.AutoField(primary_key=True)
+    codigo=models.IntegerField()
+    fecha=models.DateField()
+    nombre_especialista=models.CharField(max_length=100)
+    diagnostico=models.TextField()
+    procedimiento=models.TextField()
+    tratamiento=models.TextField()
+    paciente_id=models.IntegerField()
+    
