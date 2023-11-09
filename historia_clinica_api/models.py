094f6ccdf3620d727402c2dbd075ce2e67e75910
Daniel Villota <danielfernandovillota@gmail.com>
2023-11-09 15:46:31 -0500

    tests creation

--- historia_clinica_api/models.py
+++ historia_clinica_api/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Historia(models.Model):
+    
+    texto_validator = RegexValidator(
+        regex=r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$',
+        message='Este campo solo debe contener letras.'
+    )
+    
     id=models.AutoField(primary_key=True)
-    codigo=models.CharField(max_length=4)
-    fecha=models.DateField()
-    nombre_especialista=models.CharField(max_length=100)
+    codigo=models.CharField(max_length=4, null=False)
+    fecha=models.DateField(null=False)
+    nombre_especialista=models.CharField(max_length=20, null=False, validators=[texto_validator])
     diagnostico=models.TextField()
     procedimiento=models.TextField()
     tratamiento=models.TextField()
---
+++ historia_clinica_api/tests/test_registro_historia.py
@@ -0,0 +1,70 @@
+import unittest
+from ..models import Historia
+from ..serializer import HistoriaSerializer
+from django.test import Client
+import json
+import os
+
+class registroHistoria(unittest.TestCase):
+    
+    def tearDown(self):
+        Historia.objects.all().delete()
+
+    def setUp(self):
+        self.client = Client()
+        self.url = '/api/v1/historias'
+        file_path = os.path.join(os.path.dirname(__file__), './response/historia.json')
+        self.assert_data = json.loads(open(file_path, encoding='utf-8').read())
+
+    def test_registro_historia(self):
+        response = self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 201)
+        
+    def test_codigo_longitud(self):
+        self.tearDown()
+        Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        self.assert_data['codigo']="22222"
+        response=self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 400)
+        
+    def test_codigo_campo_obligatorio(self):
+        self.tearDown()
+        Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        self.assert_data['codigo']=""
+        response=self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_nombre_especialista_longitud(self):
+        self.tearDown()
+        Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        self.assert_data['nombre_especialista'] = "longitud de texto mayor a 20 caracteres"
+        response = self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_campo_obligatorio_nombre_especialista(self):
+        self.tearDown()
+        Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        self.assert_data['nombre_especialista'] = ""
+        response = self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 400)
+        
+    def test_nombre_especialista_solo_caracteres(self):
+        self.tearDown()
+        Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        self.assert_data['nombre_especialista'] = 15641
+        response = self.client.post(self.url, self.assert_data)
+        self.assertEqual(response.status_code, 400)
+    
+    #corregir test  
+    def test_actualizar_historia(self):
+        historia = Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        datos_actualizados = {
+            "nombre_especialista": "Maria Liz"
+        }
+        response = self.client.put(f"{self.url}/{historia.id}", json.dumps(datos_actualizados), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_eliminar_historia(self):
+        historia = Historia.objects.create(id=1, codigo='2222', fecha='2023-11-08', nombre_especialista='cristian torres', diagnostico='Prueba', procedimiento='Prueba',tratamiento='Prueba', paciente_id=6)
+        response = self.client.delete(f"{self.url}/{historia.id}/")
+        self.assertEqual(response.status_code, 204)
