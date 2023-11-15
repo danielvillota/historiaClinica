dadeba7bd8b92d06f2f33f51d93dab73db15e727
Daniel Villota <danielfernandovillota@gmail.com>
2023-11-15 16:14:58 -0500

    change database to mysql

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'historia_clinica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
+        'NAME': 'historia',
+        'OPTIONS': {
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
